4c396e5b8c5733662d6bf8f5d081140352a9e700
Budi Kuncoro <budikuncoro347@gmail.com>
2026-01-14 21:31:34 +0700

    fix mobile navbar alignment

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-alert("JavaScript berhasil terhubung!");
-const heroImg = document.querySelector(".hero-img");
+// JavaScript siap dipakai
+console.log("JS loaded");
 
-window.addEventListener("scroll", () => {
-  const scrollY = window.scrollY;
-  heroImg.style.transform = `translateY(${scrollY * 0.2}px)`;
+const menuToggle = document.getElementById("menu-toggle");
+const navLinks = document.getElementById("nav-links");
+
+menuToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
+
+/* auto close when click menu */
+navLinks.querySelectorAll("a").forEach(link => {
+  link.addEventListener("click", () => {
+    navLinks.classList.remove("active");
+  });
 });
